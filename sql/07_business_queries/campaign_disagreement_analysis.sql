0fa2787c0ce64e377459dea078b681d0540f4daf
vamsikrish69 <vamsikrisshna06@gmail.com>
2026-07-17 03:57:54 +0530

    Add campaign disagreement analysis SQL query

---
+++ sql/07_business_queries/campaign_disagreement_analysis.sql
@@ -0,0 +1,65 @@
+SELECT
+    CAMPAIGN_ID,
+    CAMPAIGN_NAME,
+    CHANNEL,
+
+    ROUND(
+        SUM(
+            CASE
+                WHEN ATTRIBUTION_MODEL = 'first_touch'
+                THEN ATTRIBUTED_REVENUE
+                ELSE 0
+            END
+        ),
+        2
+    ) AS FIRST_TOUCH_REVENUE,
+
+    ROUND(
+        SUM(
+            CASE
+                WHEN ATTRIBUTION_MODEL = 'last_touch'
+                THEN ATTRIBUTED_REVENUE
+                ELSE 0
+            END
+        ),
+        2
+    ) AS LAST_TOUCH_REVENUE,
+
+    ROUND(
+        SUM(
+            CASE
+                WHEN ATTRIBUTION_MODEL = 'linear'
+                THEN ATTRIBUTED_REVENUE
+                ELSE 0
+            END
+        ),
+        2
+    ) AS LINEAR_REVENUE
+
+FROM ANALYTICS.FCT_ATTRIBUTION_PERFORMANCE
+
+GROUP BY
+    CAMPAIGN_ID,
+    CAMPAIGN_NAME,
+    CHANNEL
+
+ORDER BY
+    ABS(
+        SUM(
+            CASE
+                WHEN ATTRIBUTION_MODEL = 'first_touch'
+                THEN ATTRIBUTED_REVENUE
+                ELSE 0
+            END
+        )
+        -
+        SUM(
+            CASE
+                WHEN ATTRIBUTION_MODEL = 'last_touch'
+                THEN ATTRIBUTED_REVENUE
+                ELSE 0
+            END
+        )
+    ) DESC
+
+LIMIT 20;
